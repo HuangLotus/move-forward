9c483920275e64154f07b16828070c72b0560c61
黄芙蓉 <huangfurong@didiglobal.com>
2020-09-19 22:39:40 +0800

    deploy blog

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy blog'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-git push -f https://github.com/HuangLotus/move-forward.git master:gh-pages
+git push -f https://github.com/HuangLotus/move-forward.git master:master
 
 cd -
