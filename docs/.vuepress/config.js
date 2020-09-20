6ee3c328a6a3dbfbe9408e9f7ae8bf7e9cf06c6d
黄芙蓉 <huangfurong@didiglobal.com>
2020-09-20 11:25:13 +0800

    删除编译产物，并提交变更

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,26 +27,29 @@ module.exports = {
                 title: '踩坑经验',   // 必要的
                 path: '/summary',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
                 collapsable: false, // 可选的, 默认值是 true,
-                sidebarDepth: 1,    // 可选的, 默认值是 1
+                // sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: [
-                    '/summary/不清楚的点梳理'
-                ]
+                    '/summary/那些不清楚的知识点梳理'
+                ],
+                initialOpenGroupIndex: 1
             },
             {
                 title: '平台架构',
-                path: '/platform',    
+                path: '/platform',
                 children: [
                     '/platform/如何规划一个node平台'
                 ],
-                collapsable: false, 
+                collapsable: false,
                 initialOpenGroupIndex: 1
             },
             {
                 title: '学习笔记',
-                path: '/mynote',    
+                path: '/mynote', 
                 children: [
+                    '/mynote/理解vue核心概念',
                     '/mynote/对webpack热更新的理解',
-                    '/mynote/理解vue核心概念'
+                    '/summary/那些不清楚的知识点梳理',
+                    '/platform/如何规划一个node平台'
                 ],
                 collapsable: false, 
                 initialOpenGroupIndex: 1 
