9987ee8e232cc67f479224079d6f1426fff19d45
huangfurong <huangfurong@360.cn>
2020-09-26 15:58:49 +0800

    删除编译产物

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy blog'
+git commit -m '提交改动，重新部署'
 
 # 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
